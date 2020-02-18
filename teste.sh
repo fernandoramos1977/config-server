d8e1dc598b8e2e02a817b90d41c77cbc5384d217
fernandoramos1977 <fernandoramos.dev@gmail.com>
2020-02-18 11:09:26 -0300

    inclusão devops branch feature-devops

--- teste.sh
+++ teste.sh
@@ -2,7 +2,11 @@
 <<<<<<< Updated upstream
 2. Ansible
 3. Kubernetes: Orquestração de microserviços
+<<<<<<< HEAD
 =======
 2. Ansible:Infra ágil configurada como código
 3. Kubernetes
 >>>>>>> Stashed changes
+=======
+4. Devops: Arquitetura, Pipeline e Monitoração
+>>>>>>> inclusão devops branch feature-devops
