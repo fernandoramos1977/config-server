1ca8b917849b80e40c0fb55281861bb579f9c724
fernandoramos1977 <fernandoramos.dev@gmail.com>
2020-02-18 10:53:59 -0300

    ajuste do nome do curso de Ansible

--- teste.sh
+++ teste.sh
@@ -1,3 +1,8 @@
 1. Git
+<<<<<<< Updated upstream
 2. Ansible
 3. Kubernetes: Orquestração de microserviços
+=======
+2. Ansible:Infra ágil configurada como código
+3. Kubernetes
+>>>>>>> Stashed changes
